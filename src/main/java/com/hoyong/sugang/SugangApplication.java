60c9c58fdbb2b23653ab17127037f35244c19242
ErranderLee <dre12am14@gmail.com>
2023-05-28 17:28:27 +0900

    first commit

---
+++ src/main/java/com/hoyong/sugang/SugangApplication.java
@@ -0,0 +1,13 @@
+package com.hoyong.sugang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugangApplication.class, args);
+	}
+
+}
