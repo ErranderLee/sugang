80584c8b54ef39761eae93d9af198e1200531e26
ErranderLee <dre12am14@gmail.com>
2023-06-18 16:41:46 +0900

    fix: 비관적 락을 사용하여 문제 해결

--- src/main/java/com/hoyong/sugang/subject/repository/SubjectRepository.java
+++ src/main/java/com/hoyong/sugang/subject/repository/SubjectRepository.java
@@ -1,10 +1,16 @@
 package com.hoyong.sugang.subject.repository;
 
 import com.hoyong.sugang.subject.entity.Subject;
+import java.util.Optional;
+import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
-
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @Transactional
+  Optional<Subject> findById(Long id);
 }
